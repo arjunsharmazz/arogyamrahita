53a00da02a99b626075a14abc0d4cbd2473f9ee7
Arjun Sharma <arjunsharma3505@gmail.com>
2026-04-10 11:54:20 +0530

    first commit

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import styles from "../css/Product.module.css";
+import styles from "../css/product.module.css";
 import { useCart } from "../context/CartContext";
 import { useAuth } from "../context/AuthContext";
 import { toast } from "react-toastify";
